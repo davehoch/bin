aac6147adda051f2baa64b04ffca51db0f126591
David Hochhauser <dhochhauser@clearwateranalytics.com>
2020-06-12 09:07:04 -0600

    add NetBeans log files to deleteTempFiles.sh

--- deleteTempFiles.sh
+++ deleteTempFiles.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 echo deleting tomcat logs
 deleteTomcatLogs.sh
+
+echo deleting NetBeans log files
+rm -f ~/Library/Application\ Support/NetBeans/11.3/var/log/*
+rm -f ~/Library/Application\ Support/NetBeans/12.0/var/log/*
