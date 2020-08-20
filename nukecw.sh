c09fa606b79d8f81cdd75baa9e3fef4dae275e41
David Hochhauser <dhochhauser@clearwateranalytics.com>
2020-08-20 13:23:59 -0600

    adding nukenetbeanscache and making other nuke scripts move to ~/tmp first

--- nukecw.sh
+++ nukecw.sh
@@ -3,9 +3,9 @@ echo nuking CW in .m2
 # these folders can be large, so it's better to rename then delete
 
 # rename the folders
-mv ~/.m2/repository/com/ca ~/.m2/repository/com/ca-DeleteMe
-mv ~/.m2/repository/com/clearwateranalytics ~/.m2/repository/com/clearwateranalytics-DeleteMe
+mv ~/.m2/repository/com/ca ~/tmp/ca_DeleteMe
+mv ~/.m2/repository/com/clearwateranalytics ~/tmp/clearwateranalytics_DeleteMe
 
-# delete the renamed folders
-rm -r ~/.m2/repository/com/ca-DeleteMe
-rm -r ~/.m2/repository/com/clearwateranalytics-DeleteMe
+# delete the renamed folders async
+rm -r ~/tmp/ca_DeleteMe &
+rm -r ~/tmp/clearwateranalytics_DeleteMe &
