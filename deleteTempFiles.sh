9480c0db7170bdbcc3432efa6951eeda55ba89b7
David Hochhauser <dhochhauser@clearwateranalytics.com>
2021-01-14 10:07:05 -0700

    updating paths for updated Tomcat and NetBeans

--- deleteTempFiles.sh
+++ deleteTempFiles.sh
@@ -2,5 +2,4 @@
 deleteTomcatLogs.sh
 
 echo deleting NetBeans log files
-rm -f ~/Library/Application\ Support/NetBeans/11.3/var/log/*
-rm -f ~/Library/Application\ Support/NetBeans/12.0/var/log/*
+rm -f ~/Library/Application\ Support/NetBeans/12.2/var/log/*
