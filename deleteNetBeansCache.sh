34b399b57a1e99f08379f3844b0909ab82d6ed4e
David Hochhauser <dhochhauser@clearwateranalytics.com>
2021-08-27 14:46:28 -0600

    updating deleteNetBeansCache for NetBeans 12.3

--- deleteNetBeansCache.sh
+++ deleteNetBeansCache.sh
@@ -1,6 +1,6 @@
-echo nuking NetBeans 12.2 cache dir
+echo nuking NetBeans 12.3 cache dir
 
 # this folder can be huge, so rename, and then delete async
 # todo - rm everything under /NetBeans/*
-mv ~/Library/Caches/NetBeans/12.2 ~/tmp/NetBeansCache_DeleteMe
+mv ~/Library/Caches/NetBeans/12.3 ~/tmp/NetBeansCache_DeleteMe
 rm -r ~/tmp/NetBeansCache_DeleteMe &
