50c669870c9f667ca2f0790f1c250b1a79143e2b
David Hochhauser <dhochhauser@clearwateranalytics.com>
2022-05-19 20:39:13 -0600

    adding comments to skingthecat.sh

--- skinthecat.sh
+++ skinthecat.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Could this be written more simply using pgrep and/or pkill?
+
 # grep -v grep <- This removes the process that's grepping for tomcat
 echo killing tomcat
 ps -ef | grep tomcat | grep -v grep | awk '{print $2}' | xargs kill -9
