b1d39513d2b7d99dd2899855f32247d493033a5f
David Hochhauser <dhochhauser@clearwateranalytics.com>
2022-06-17 15:54:49 -0600

    creating rmasync, and updating deleteM2 scripts to use it

--- deleteM2CW.sh
+++ deleteM2CW.sh
@@ -3,14 +3,6 @@ set -e              # Exit immediately if a pipeline returns a non-zero status.
 set -u              # Treat unset variables and parameters other than the special parameters ‘@’ or ‘*’ as an error when performing parameter expansion.
 set -o pipefail     # Return value of a pipeline is the value of the last (rightmost) command to exit with a non-zero status, or zero if all commands in the pipeline exit successfully. 
 
-echo nuking CW in .m2
-
 # these folders can be large, so it's better to rename then delete
-
-# rename the folders
-mv ~/.m2/repository/com/ca ~/tmp/ca_DeleteMe
-mv ~/.m2/repository/com/clearwateranalytics ~/tmp/clearwateranalytics_DeleteMe
-
-# delete the renamed folders async
-rm -r ~/tmp/ca_DeleteMe &
-rm -r ~/tmp/clearwateranalytics_DeleteMe &
+rmasync ~/.m2/repository/com/ca
+rmasync ~/.m2/repository/com/clearwateranalytics
