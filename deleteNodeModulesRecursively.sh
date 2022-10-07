cc213dcdc25d1f202944f9c148a6aa0cf3e97eb8
David Hochhauser <dhochhauser@clearwateranalytics.com>
2022-10-07 15:35:07 -0600

    used shfmt to reformat all files

--- deleteNodeModulesRecursively.sh
+++ deleteNodeModulesRecursively.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
-set -e              # Exit immediately if a pipeline returns a non-zero status.
-set -u              # Treat unset variables and parameters other than the special parameters ‘@’ or ‘*’ as an error when performing parameter expansion.
-set -o pipefail     # Return value of a pipeline is the value of the last (rightmost) command to exit with a non-zero status, or zero if all commands in the pipeline exit successfully. 
+set -e          # Exit immediately if a pipeline returns a non-zero status.
+set -u          # Treat unset variables and parameters other than the special parameters ‘@’ or ‘*’ as an error when performing parameter expansion.
+set -o pipefail # Return value of a pipeline is the value of the last (rightmost) command to exit with a non-zero status, or zero if all commands in the pipeline exit successfully.
 
-for item in `ls`; do
-    if [[ -d $item ]]; then
-        cd $item
-        if [[ -d node_modules ]]; then
-            echo "rm -rf $item/node_modules/"
-            rm -rf node_modules/
-        fi
-        if [[ -e pom.xml ]]; then
-            echo "Maven Clean on $item"
-            mvn -q clean
-        fi
-        cd ..
-    fi
+for item in $(ls); do
+	if [[ -d $item ]]; then
+		cd $item
+		if [[ -d node_modules ]]; then
+			echo "rm -rf $item/node_modules/"
+			rm -rf node_modules/
+		fi
+		if [[ -e pom.xml ]]; then
+			echo "Maven Clean on $item"
+			mvn -q clean
+		fi
+		cd ..
+	fi
 done
