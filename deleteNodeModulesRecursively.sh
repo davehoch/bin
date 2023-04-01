269bd8061b5670e6ff74d6d3436ea5ca1d56a2ce
David Hochhauser <dhochhauser@clearwateranalytics.com>
2023-04-01 10:19:11 -0600

    formatted all files with shfmt --indent 2 --case-indent --write --simplify [^R]*

--- deleteNodeModulesRecursively.sh
+++ deleteNodeModulesRecursively.sh
@@ -4,16 +4,16 @@ set -u          # Treat unset variables and parameters other than the special pa
 set -o pipefail # Return value of a pipeline is the value of the last (rightmost) command to exit with a non-zero status, or zero if all commands in the pipeline exit successfully.
 
 for item in $(ls); do
-	if [[ -d $item ]]; then
-		cd $item
-		if [[ -d node_modules ]]; then
-			echo "rm -rf $item/node_modules/"
-			rm -rf node_modules/
-		fi
-		if [[ -e pom.xml ]]; then
-			echo "Maven Clean on $item"
-			mvn -q clean
-		fi
-		cd ..
-	fi
+  if [[ -d $item ]]; then
+    cd $item
+    if [[ -d node_modules ]]; then
+      echo "rm -rf $item/node_modules/"
+      rm -rf node_modules/
+    fi
+    if [[ -e pom.xml ]]; then
+      echo "Maven Clean on $item"
+      mvn -q clean
+    fi
+    cd ..
+  fi
 done
